c195ab0acff6b2b966a4cc170eb8a8e9872a7e20
matheusmiranda96 <matth.miranda@gmail.com>
2024-09-08 02:26:26 -0300

    beckend-nodejs
    Realizada configuração da api.

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express  = require ('express');
+const routes = require('./routes');
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+});
