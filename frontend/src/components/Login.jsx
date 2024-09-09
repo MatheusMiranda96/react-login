4d24aa1a0a08f81209faf3c957ebbfcfb7f0b8d0
matheusmiranda96 <matth.miranda@gmail.com>
2024-09-09 16:39:14 -0300

    frontend
    realizada conexão da API com o Front-end.

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import axios from 'axios';
+
+function Login() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        const response = await axios.post ('http://localhost:3000/login',
+            JSON.stringify({email, password}),
+            {
+                headers: { 'Content-Type' : 'application/json' }
+
+                
+                }
+        )
+        console.log('login efetuado')
+    }
+
+    return(
+      <div className="login-form-wrap">
+          <h2>Login</h2>
+          <form className="login-form">
+            
+            <input type="email" 
+            name="email" 
+            placeholder='email' 
+            required 
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            
+            <input type="password" 
+            name="password" 
+            placeholder='senha' 
+            required
+            onChange={(e) => setPassword(e.target.value)} 
+            />
+            
+            <button type="submit" 
+            className='btn-login'
+            onClick={(e) => handleLogin(e)}>Login</button>
+            
+          </form>
+        </div>
+    )
+  }
+
+  export default Login
